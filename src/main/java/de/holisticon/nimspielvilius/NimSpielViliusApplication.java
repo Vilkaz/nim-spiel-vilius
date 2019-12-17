f71933fd9921f0910bf917dbd208e030b59016bd
Vilius Kukanauskas <Vilius.Kukanauskas@tui.com>
2019-12-17 15:47:13 +0100

    Initial commit

---
+++ src/main/java/de/holisticon/nimspielvilius/NimSpielViliusApplication.java
@@ -0,0 +1,13 @@
+package de.holisticon.nimspielvilius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimSpielViliusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimSpielViliusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/holisticon/nimspielvilius/NimSpielViliusApplicationTests.java
@@ -0,0 +1,13 @@
+package de.holisticon.nimspielvilius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimSpielViliusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
