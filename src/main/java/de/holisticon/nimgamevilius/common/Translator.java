ceac8e02e83e2b3f349ca1e4fe57878579dcbc13
vilius.kukanauskas@gmail.com <GitHubCom123123123!!!>
2020-02-25 11:25:03 +0100

    removed Translator from Spring content

--- src/main/java/de/holisticon/nimgamevilius/common/Translator.java
+++ src/main/java/de/holisticon/nimgamevilius/common/Translator.java
@@ -1,30 +1,21 @@
 package de.holisticon.nimgamevilius.common;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.stereotype.Component;
-
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  * Gets the Strings from the language propperty file.
- * For Demo, hardcoded in english.
+ * For Demo, the locale is  hardcoded in english.
  */
-@Component
 public class Translator {
-
-    private static ResourceBundleMessageSource messageSource;
-
-    @Autowired
-    Translator(ResourceBundleMessageSource messageSource) {
-        Translator.messageSource = messageSource;
-    }
 
     /**
      * For production usage, we would change the hardcoded english
      * with LocaleSupplier.
      */
+    private static ResourceBundle messageSource = ResourceBundle.getBundle("messages", Locale.ENGLISH);
+
     public static String getText(String msg) {
-        return messageSource.getMessage(msg, null, Locale.ENGLISH);
+        return messageSource.getString(msg);
     }
 }
